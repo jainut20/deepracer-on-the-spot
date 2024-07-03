754125536fe7533e49ce6b230c1d76b67286640d
Sumit Chouhan <sumit_jiet@yahoo.com>
2024-07-03 16:23:20 +0100

    first

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,33 +1,41 @@
+# This example determines how far away the agent is from the center line and gives higher reward
+# if it is closer to the center of the track. It will incentivize the agent to closely follow the
+# center line.
+
 def reward_function(params):
     '''
-    Example of penalize steering, which helps mitigate zig-zag behaviors
+    Example of rewarding the agent to follow center line
     '''
     
     # Read input parameters
-    distance_from_center = params['distance_from_center']
     track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
-
-    # Calculate 3 marks that are farther and father away from the center line
+    distance_from_center = params['distance_from_center']
+    progress = params['progress']
+    steps = params['steps']
+    benchmark_time=14.7
+    is_offtrack=params['is_offtrack']
+    
+    # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
+    
+    if progress == 100:
+        if round(steps/15,1)<benchmark_time:
+            reward+=100*round(steps/15,1)/benchmark_time
+        else:
+            reward += 100
+    elif is_offtrack:
+        reward-=50 
 
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1
+        reward = 1.0
     elif distance_from_center <= marker_2:
         reward = 0.5
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
         reward = 1e-3  # likely crashed/ close to off track
-
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
-
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
-
+    
     return float(reward)
