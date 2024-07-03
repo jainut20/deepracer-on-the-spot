3d6e48af539b48d8094d75fe8695cf40ce814a5d
Sumit Chouhan <sumit_jiet@yahoo.com>
2024-07-03 16:46:32 +0100

    reverting to basic func since getting format error

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -10,24 +10,12 @@ def reward_function(params):
     # Read input parameters
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    progress = params['progress']
-    steps = params['steps']
-    benchmark_time=14.7
-    is_offtrack=params['is_offtrack']
     
     # Calculate 3 markers that are at varying distances away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
     
-    if progress == 100:
-        if round(steps/15,1)<benchmark_time:
-            reward+=100*round(steps/15,1)/benchmark_time
-        else:
-            reward += 100
-    elif is_offtrack:
-        reward-=50 
-
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
         reward = 1.0
