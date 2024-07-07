4a4529179facebff875c4ba35ca4a7dd6c33e79f
jainut20 <utkarsh.vipin.jain@gmail.com>
2024-07-07 18:45:07 +0100

    Bow tie track code

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,23 +1,28 @@
 def reward_function(params):
     '''
-    Example of penalize steering, which helps mitigate zig-zag behaviors
+    Example of reward function using centre line and progress
     '''
-    
-    # Read input parameters
-    reward = 0
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
 
-    # Calculate 3 marks that are farther and father away from the center line
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    progress = params['progress']
+    steps = params['steps']
+    speed = params['speed']
+    all_wheels_on_track = params['all_wheels_on_track']
+    is_offtrack = params['is_offtrack']
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+
+    # Reward for staying in the center of the track
+    reward = 1.0
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
 
-    
-    # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1
+        reward = 1.0
     elif distance_from_center <= marker_2:
         reward = 0.5
     elif distance_from_center <= marker_3:
@@ -25,50 +30,21 @@ def reward_function(params):
     else:
         reward = 1e-3  # likely crashed/ close to off track
 
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
+    # Higher reward if the car completes a lap faster
+    if progress == 100:
+        reward += 10.0
 
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
-        
-     
-    # # STAY ON LANE NOT CRASHING   
-    # all_wheels_on_track = params['all_wheels_on_track']
-    # distance_from_center = params['distance_from_center']
-    # track_width = params['track_width']
-    # objects_distance = params['objects_distance']
-    # _, next_object_index = params['closest_objects']
-    # objects_left_of_center = params['objects_left_of_center']
-    # is_left_of_center = params['is_left_of_center']
+    # Penalise if the car goes off-track
+    if not all_wheels_on_track:
+        reward = 1e-3
 
-    # # Initialize reward with a small number but not zero
-    # # because zero means off-track or crashed
+    # Reward for speed
+    speed_threshold = 1.0
+    if speed > speed_threshold:
+        reward += 1.0
 
-    # # Reward if the agent stays inside the two borders of the track
-    # if all_wheels_on_track and (0.5 * track_width - distance_from_center) >= 0.05:
-    #     reward_lane = 1.0
-    # else:
-    #     reward_lane = 1e-3
-
-    # # Penalize if the agent is too close to the next object
-    # reward_avoid = 1.0
-
-    # # Distance to the next object
-    # distance_closest_object = objects_distance[next_object_index]
-    # # Decide if the agent and the next object is on the same lane
-    # is_same_lane = objects_left_of_center[next_object_index] == is_left_of_center
-
-    # if is_same_lane:
-    #     if 0.5 <= distance_closest_object < 0.8: 
-    #         reward_avoid *= 0.5
-    #     elif 0.3 <= distance_closest_object < 0.5:
-    #         reward_avoid *= 0.2
-    #     elif distance_closest_object < 0.3:
-    #         reward_avoid = 1e-3 # Likely crashed
-
-    # # Calculate reward by putting different weights on 
-    # # the two aspects above
-    # reward += 1.0 * reward_lane + 4.0 * reward_avoid
+    # Penalise if the car is off-track
+    if is_offtrack:
+        reward = 1e-3
 
     return float(reward)
